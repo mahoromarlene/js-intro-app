d4585b02336ae36a7ab43095eed21e056d06dd37
Marlene Uwamahoro <uwamahoromarlene@gmail.com>
2017-02-23 10:45:20 +0200

    Create a click boxes game

--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -1,1 +1,11 @@
 
+var count = 1
+function jackhammer() {
+  if (count === 5)
+    alert("You win!");
+  } else {
+    console.log("Click again");
+    count++
+  }
+}
+
--- app/assets/javascripts/three.js
+++ app/assets/javascripts/three.js
@@ -1,1 +1,15 @@
+var clickBox1 = 0
+var clickBox2 = 0
+var clickBox3 = 0
 
+function ClickThreeBoxes(clickBox){
+  if (clickBox1 === 1 && clickBox2 === 1 && clickBox3 === 1){
+    alert("You win");
+  } else if (clickBox === clickBox1){
+    console.log("Click on another one");
+  } else if (clickBox === clickBox2){
+    console.log("Click on another one");
+  } else if (clickBox = clickBox3){
+    console.log("Click on another one");
+  }
+}
